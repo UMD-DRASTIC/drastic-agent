1267aed28631a2d2d661546e00fb7d3577f4e326
Ross Jones <ross@servercode.co.uk>
2015-07-03 17:05:14 +0100

    Useful gevent actor class

---
+++ agent/actor.py
@@ -0,0 +1,21 @@
+import gevent
+
+class Actor(gevent.Greenlet):
+
+    def __init__(self):
+        self.inbox = queue.Queue()
+        Greenlet.__init__(self)
+
+    def recieve(self, message):
+        """
+        Define in your subclass.
+        """
+        raise NotImplemented()
+
+    def _run(self):
+        self.running = True
+
+        while self.running:
+            message = self.inbox.get()
+            self.recieve(message)
+
